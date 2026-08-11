a00ef86315bfd9904705d23249c9815f6dc5e1ab
Hardik Chilana <h2154678@gmail.com>
2026-08-11 15:25:13 +0530

    Time: 2 ms (40.93%) | Memory: 47.5 MB (31.38%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,29 +1,26 @@
 class Solution {
-    static public int[] parent;
-    public int find(int a) {
-        if(parent[a]==a)return a;
-       return find(parent[a]);
-    }
-    public void union(int a,int b) {
-        int leaderA = find(a);
-        int leaderB = find(b);
-        if(parent[a] != parent[b]) {
-            parent[leaderB] = leaderA;
-        }
-    }
     public int findCircleNum(int[][] isConnected) {
-        int n =isConnected.length;
-        parent = new int[n+1];
-        for(int i=1;i<=n;i++) parent[i] = i;
+        int n = isConnected.length;
+        int m = isConnected[0].length;
+        boolean v[] = new boolean[n];
+        int c = 0;
         for(int i=0;i<n;i++) {
-            for(int j=0;j<n;j++) {
-                if(i!=j && isConnected[i][j]==1)union(i+1,j+1);
+            for(int j=0;j<m;j++){
+                if(!v[i]) {
+                    bfs(i,isConnected,v);
+                    c++;
+                }
             }
         }
-        int count = 0;
-        for(int i=1;i<=n;i++) {
-            if(parent[i]==i)count++;
+        return c;
+    }
+    public void bfs(int s , int[][] isConnected , boolean v[]) {
+        v[s] = true;
+        for(int i=0;i<isConnected.length;i++) {
+            if(v[i]!= true && isConnected[s][i] == 1) {
+                v[i] = true;
+                bfs(i,isConnected,v);
+            }
         }
-        return count;
     }
 }
